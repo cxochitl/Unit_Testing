082f96b6fd8192171a94b96cfda31aff7398247f
cxochitl <61166084+cxochitl@users.noreply.github.com>
2020-10-30 14:53:25 -0600

    Add code files

---
+++ Smart_Conv.c
@@ -0,0 +1,65 @@
+/* Includes to include */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*Function Declarations*/
+
+float hex_to_vel(int hex);
+float hex_to_dec(int hex);
+float hex_to_angle(int hex);
+
+
+/*Definitions*/
+#define MaxVelValue  40.0F
+#define MaxVelInHex 28
+#define MaxAngValue  360.0F
+#define MaxAngInHex 168.0F
+
+/*Function to convert Bits to velocity in km/hr */
+float hex_to_vel(int hex)
+{
+	float Vel = 0.0F;
+
+	if (hex >= MaxVelInHex)
+	{
+		Vel = MaxVelValue;
+	}
+	else if (hex < 0.0F)
+	{
+		Vel = 0.0F;
+	}
+	else
+	{
+		Vel = hex_to_dec(hex);
+	}
+
+	return Vel;
+}
+
+float hex_to_angle(int hex) 
+{
+	float Angle = 0.0F;
+	Angle = hex_to_dec(hex);
+	
+	if (Angle >= MaxAngValue)
+	{
+		Angle = MaxAngValue;
+	}
+	else if (Angle <= 0.0F)
+	{
+		Angle = 0.0F;
+	}
+
+
+	return Angle;
+
+}
+
+float hex_to_dec (int hex)
+{
+	float decimal = 0.0F;
+	decimal = (float)(hex);
+	return decimal;
+}
+
---
+++ test.cpp
@@ -0,0 +1,65 @@
+#include "pch.h"
+#include "Smart_Conv.c"
+
+
+/*First, test velocity*/
+/*Upper limit*/
+TEST(test_velocity_case, HexToVelocityMax) {
+	EXPECT_NEAR(40.0F, hex_to_vel(0x28), 0.01);
+}
+
+/*Out of upper limit*/
+TEST(test_velocity_case, HexToVelocityOutUpperLimit) {
+	EXPECT_NEAR(40.0F, hex_to_vel(0x2A0), 0.01);
+}
+
+/*Lower limit*/
+TEST(test_velocity_case, HexToVelocityLow) {
+	EXPECT_NEAR(0.0F, hex_to_vel(0x00), 0.01);
+}
+
+/*Out of lower limit*/
+TEST(test_velocity_case, HexToVelocityOutLowerLimit) {
+	EXPECT_NEAR(0.0F, hex_to_vel(-0x200), 0.01);
+}
+
+/*With letters*/
+TEST(test_velocity_case, HexToVelocityLetter) {
+	EXPECT_NEAR(10.0F, hex_to_vel(0xA), 0.01);
+}
+
+/*With incorrect input*/
+TEST(test_velocity_case, HexToVelocityDecimal) {
+	EXPECT_NEAR(3.0F, hex_to_vel(3.6), 0.01);
+}
+
+/*Second, test angle*/
+/*Upper limit*/
+TEST(test_angle_case, HexToAngleMax) {
+	EXPECT_NEAR(360.0F, hex_to_angle(0x168), 0.01);
+}
+
+/*Out of upper limit*/
+TEST(test_angle_case, HexToAngleOutUpperLimit) {
+	EXPECT_NEAR(360.0F, hex_to_angle(0x200), 0.01);
+}
+
+/*Lower limit*/
+TEST(test_angle_case, HexToAngleLow) {
+	EXPECT_NEAR(0.0F, hex_to_angle(0x00), 0.01);
+}
+
+/*Out of lower limit*/
+TEST(test_angle_case, HexToAngleOutLowerLimit) {
+	EXPECT_NEAR(0.0F, hex_to_angle(-0x20A), 0.01);
+}
+
+/*With letters*/
+TEST(test_angle_case, HexToAngleLetter) {
+	EXPECT_NEAR(171.0F, hex_to_angle(0xAB), 0.01);
+}
+
+/*With incorrect input*/
+TEST(test_angle_case, HexToAngleDecimal) {
+	EXPECT_NEAR(183.0F, hex_to_angle(183.6), 0.01);
+}
